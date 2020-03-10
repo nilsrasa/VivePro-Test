f14ea573b368b08612f5990ada2704f60f4348ad
nilsras <nils.rasamoel@gmail.com>
2020-03-10 11:27:10 +0100

    Initial commit

---
+++ Assets/Scripts/Utilities/GestureInfo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ViveHandTracking;
+
+public class GestureInfo : MonoBehaviour
+{
+    public GameObject leftRay, rightRay;
+    Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GestureProvider.Status == GestureStatus.Running)
+        {
+            string left = "null";
+            string right = "null";
+
+            if (GestureProvider.LeftHand != null)
+            {
+                left = GestureProvider.LeftHand.gesture.ToString();
+                //leftRay.SetActive(GestureProvider.LeftHand.gesture == GestureType.Point);
+            }
+            else
+            {
+                leftRay.SetActive(false);
+            }
+
+            if (GestureProvider.RightHand != null)
+            {
+                right = GestureProvider.RightHand.gesture.ToString();
+                //rightRay.SetActive(GestureProvider.RightHand.gesture == GestureType.Point);
+            }
+            else
+            {
+                rightRay.SetActive(false);
+            }
+
+            string s = $"Left hand is doing: {left} \n right hand is doing: {right}";
+            text.text = s;
+        }
+    }
+}
