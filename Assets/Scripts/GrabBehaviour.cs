086f8dd4748a1ae592d536e7300700fb3fba5ba5
nilsras <nils.rasamoel@gmail.com>
2020-03-18 21:03:47 +0100

    Grab behaviour

---
+++ Assets/Scripts/GrabBehaviour.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ViveHandTracking;
+
+class GrabBehaviour : MonoBehaviour
+{
+    private static Color enterColor = new Color(0, 0, 0.3f, 1);
+    private static Color moveColor = new Color(0, 0.3f, 0, 1);
+
+    private Rigidbody _target = null;
+    private Transform _anchor = null;
+    private int _state = 0;
+    private bool _exit = true;
+
+    void Awake()
+    {
+        var go = new GameObject("Anchor");
+        _anchor = go.transform;
+        _anchor.parent = transform;
+    }
+
+    void Update()
+    {
+        if (_state == 1 && _target != null)
+        {
+            _anchor.position = transform.position;
+            if (GestureProvider.HaveSkeleton)
+                _anchor.rotation = transform.rotation;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.name.StartsWith("Cube"))
+            return;
+
+        var newTarget = other.GetComponent<Rigidbody>();
+        if (newTarget == _target)
+        {
+            _exit = false;
+            return;
+        }
+        if (_target != null && _state == 1)
+            StopMove();
+        _target = other.GetComponent<Rigidbody>();
+        if (_target != null)
+        {
+            _exit = false;
+            if (_state == 1)
+                StartMove();
+            else
+                SetColor(false);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>() != _target)
+            return;
+        if (_state == 1)
+            _exit = true;
+        else
+        {
+            SetColor(null);
+            _target = null;
+        }
+    }
+
+    public void OnStateChanged(int state)
+    {
+        this._state = state;
+        if (_target == null)
+            return;
+        if (state == 1)
+            StartMove();
+        else if (state == 0)
+        {
+            StopMove();
+            if (_exit)
+                _target = null;
+            else
+                SetColor(false);
+        }
+    }
+
+    void StartMove()
+    {
+        _target.useGravity = false;
+        _target.isKinematic = true;
+        _target.GetComponent<Grabbable>().OnGrab(_anchor);
+        //_anchor.SetParent(_target.transform.parent, true);
+        //_target.transform.SetParent(_anchor, true);
+        SetColor(true);
+    }
+
+    void StopMove()
+    {
+        //_target.transform.SetParent(_anchor.parent, true);
+        //_anchor.parent = transform;
+        _target.GetComponent<Grabbable>().OnRelease();
+        _target.useGravity = true;
+        _target.isKinematic = false;
+        SetColor(null);
+    }
+
+    // true: moving, false: touching, null: not touched
+    void SetColor(bool? moving)
+    {
+        if (_target == null)
+            return;
+        var renderer = _target.GetComponent<Renderer>();
+        if (renderer == null)
+            return;
+        var material = renderer.material;
+        if (moving == null)
+        {
+            material.DisableKeyword("_EMISSION");
+            return;
+        }
+        material.EnableKeyword("_EMISSION");
+        material.SetColor("_EmissionColor", moving.Value ? moveColor : enterColor);
+    }
+}
+
---
+++ Assets/Scripts/Grabbable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : MonoBehaviour
+{
+    private Transform _target;
+
+    private bool _isGrabbed = false;
+
+    [SerializeField]private float _moveSpeed = 6f;
+
+    [SerializeField] private float _turnSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = new GameObject("AnchorPoint").transform;
+        _target.SetParent(transform);
+        _target.localPosition = Vector3.zero;
+        _target.localRotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isGrabbed)
+        {
+            transform.position = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * _moveSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, _target.rotation, Time.deltaTime * _turnSpeed);
+        }
+    }
+
+    public void OnGrab(Transform anchor)
+    {
+        _target.SetParent(anchor, true);
+        _isGrabbed = true;
+    }
+
+    public void OnRelease()
+    {
+        _isGrabbed = false;
+        _target.SetParent(transform);
+        _target.localPosition = Vector3.zero;
+        _target.localRotation = Quaternion.identity;
+    }
+}
