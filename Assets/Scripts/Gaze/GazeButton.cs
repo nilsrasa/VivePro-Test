a83ff55401951418207a7c3fbdf79b0ec741013b
nilsras <nils.rasamoel@gmail.com>
2020-04-01 17:54:27 +0200

    Added gazebutton
    UI button that is activated by gaze for a period

--- Assets/Scripts/Gaze/GazeButton.cs
+++ Assets/Scripts/Gaze/GazeButton.cs
@@ -1,18 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Tobii.G2OM;
+using Tobii.XR.Examples;
 using UnityEngine;
 
-public class GazeButton : MonoBehaviour
+[RequireComponent(typeof(UIGazeButtonGraphics))]
+public class GazeButton : MonoBehaviour, IGazeFocusable
 {
-    // Start is called before the first frame update
-    void Start()
+    // Event called when the button is clicked.
+    public UIButtonEvent OnButtonClicked;
+
+    // The state the button is currently  in.
+    private ButtonState _currentButtonState = ButtonState.Idle;
+
+    // Private fields.
+    private bool _hasFocus;
+    private UIGazeButtonGraphics _uiGazeButtonGraphics;
+
+    [SerializeField]private float _gazeTime = 1.5f;
+    private bool _isCounting;
+    private float _time;
+
+    public void GazeFocusChanged(bool hasFocus)
     {
-        
+        if (!_isCounting && hasFocus)
+        {
+            _time = _gazeTime;
+            _isCounting = true;
+        }
+        else if (_isCounting && !hasFocus)
+        {
+            _isCounting = false;
+            GazeRing.Instance.Hide();
+        }
+
+        _hasFocus = hasFocus;
+        UpdateState(_hasFocus ? ButtonState.Focused : ButtonState.Idle);
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ResetButton()
     {
-        
+        yield return new WaitForSecondsRealtime(.1f);
+        UpdateState(_hasFocus ? ButtonState.Focused : ButtonState.Idle);
+    }
+
+    private void Click()
+    {
+        UpdateState(ButtonState.PressedDown);
+        // Invoke click event.
+        if (OnButtonClicked != null)
+        {
+            OnButtonClicked.Invoke(gameObject);
+        }
+
+        StartCoroutine(ResetButton());
+    }
+
+    private void Start()
+    {
+        // Store the graphics class.
+        _uiGazeButtonGraphics = GetComponent<UIGazeButtonGraphics>();
+
+        // Initialize click event.
+        if (OnButtonClicked == null)
+        {
+            OnButtonClicked = new UIButtonEvent();
+        }
+    }
+
+    private void Update()
+    {
+        if (_isCounting)
+        {
+            _time -= Time.deltaTime;
+            if (_time > 0)
+            {
+                GazeRing.Instance.SetProgress((_gazeTime - _time) / _gazeTime);
+            }
+            else
+            {
+                _isCounting = false;
+                Click();
+                GazeRing.Instance.Hide();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Updates the button state and starts an animation of the button.
+    /// </summary>
+    /// <param name="newState">The state the button should transition to.</param>
+    private void UpdateState(ButtonState newState)
+    {
+        var oldState = _currentButtonState;
+        _currentButtonState = newState;
+
+        // Variables for when the button is pressed or clicked.
+        var buttonPressed = newState == ButtonState.PressedDown;
+        var buttonClicked = (oldState == ButtonState.PressedDown && newState == ButtonState.Focused);
+
+        // If the button is being pressed down or clicked, animate the button click.
+        if (buttonPressed || buttonClicked)
+        {
+            _uiGazeButtonGraphics.AnimateButtonPress(_currentButtonState);
+        }
+        // In all other cases, animate the visual feedback.
+        else
+        {
+            _uiGazeButtonGraphics.AnimateButtonVisualFeedback(_currentButtonState);
+        }
     }
 }
---
+++ Assets/Scripts/Gaze/GazeRing.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.XR;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GazeRing : MonoBehaviour
+{
+    private static GazeRing _instance;
+    public static GazeRing Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private float _progress;
+
+    private Image _image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _image.fillAmount = _progress;
+    }
+
+    public void SetProgress(float pct)
+    {
+        _progress = pct;
+
+        TobiiXR_GazeRay gaze = TobiiXR.GetEyeTrackingData(TobiiXR_TrackingSpace.World).GazeRay;
+        RaycastHit hit;
+        if (Physics.Raycast(gaze.Origin, gaze.Direction, out hit))
+        {
+            transform.position = hit.point;
+        }
+
+    }
+
+    public void Hide()
+    {
+        _progress = 0;
+    }
+
+    private void OnDestroy()
+    {
+        _instance = null;
+    }
+}
