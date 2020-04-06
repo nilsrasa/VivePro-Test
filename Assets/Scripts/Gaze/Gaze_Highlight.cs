9470e0a224dfe43774f6ab075c1bd213d9209f13
nilsras <nils.rasamoel@gmail.com>
2020-04-06 15:20:23 +0200

    Wokring gaze highlight
    Gazehighlight now supports combining meshes from several objects.
    Look into gaze collider settings, ex layermasks

--- Assets/Scripts/Gaze/Gaze_Highlight.cs
+++ Assets/Scripts/Gaze/Gaze_Highlight.cs
@@ -10,8 +10,8 @@ public class Gaze_Highlight : MonoBehaviour, IGazeFocusable
     private Color _color = Color.white;
     [SerializeField, Tooltip("The width of the highlight contour"), Range(0.01f, 2f)]
     private float _lineWidth = .2f;
-    [SerializeField, Tooltip("The meshfilter of the object containing the mesh (if found elsewhere)")]
-    private MeshFilter _mesh;
+    [SerializeField, Tooltip("The meshfilter/s of the object/s containing the mesh/es (if found elsewhere)")]
+    private MeshFilter[] _meshFilters;
 
     private GameObject _highlight;
     public void GazeFocusChanged(bool hasFocus)
@@ -28,9 +28,24 @@ public class Gaze_Highlight : MonoBehaviour, IGazeFocusable
     {
         //Instantiation the gameobject
         _highlight = new GameObject("Highlight");
+
+        Mesh mesh;
+        switch (_meshFilters.Length)
+        {
+            case 0:
+                mesh = GetComponent<MeshFilter>().mesh;
+                break;
+
+            case 1:
+                mesh = _meshFilters[0].mesh;
+                break;
+
+            default:
+                mesh = CombinedMeshes();
+                break;
+        }
         //Adding the meshfilter and copying the mesh from the original object
-        _highlight.AddComponent<MeshFilter>().mesh = 
-            (_mesh == null) ? GetComponent<MeshFilter>().mesh : _mesh.mesh;
+        _highlight.AddComponent<MeshFilter>().mesh = mesh;
         //Setting up the highlight material
         Material mat = Gaze_Settings.HighlightMaterial;
         mat.SetColor("g_vOutlineColor", _color);
@@ -38,9 +53,28 @@ public class Gaze_Highlight : MonoBehaviour, IGazeFocusable
         _highlight.AddComponent<MeshRenderer>().material = mat;
         //Setting the transform parent and zeroing it's rot and pos
         _highlight.transform.SetParent(transform);
-        _highlight.transform.localPosition = Vector3.zero;
-        _highlight.transform.localRotation = Quaternion.identity;
+        if (_meshFilters.Length < 1)//fixes problem where combined meshes reset origin
+        {
+            _highlight.transform.localPosition = Vector3.zero;
+            _highlight.transform.localRotation = Quaternion.identity;
+        }
+        //Remember to reset scale!
+        _highlight.transform.localScale = Vector3.one;
         //Disable the gameobject
         _highlight.SetActive(false);
+    }
+
+    private Mesh CombinedMeshes()
+    {
+        Mesh mesh = new Mesh();
+        CombineInstance[] combine = new CombineInstance[_meshFilters.Length];
+
+        for (int i = 0; i < combine.Length; i++){
+            combine[i].mesh = _meshFilters[i].sharedMesh;
+            combine[i].transform = _meshFilters[i].transform.localToWorldMatrix;
+        }
+
+        mesh.CombineMeshes(combine);
+        return mesh;
     }
 }
