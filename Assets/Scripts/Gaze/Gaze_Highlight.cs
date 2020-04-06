0c892010d9f78f0c0140db2421e9fbc2ef5da3a9
nilsras <nils.rasamoel@gmail.com>
2020-04-06 13:31:24 +0200

    New gazehighlight script

---
+++ Assets/Scripts/Gaze/Gaze_Highlight.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.G2OM;
+using UnityEngine;
+
+public class Gaze_Highlight : MonoBehaviour, IGazeFocusable
+{
+    [Header("Highlight")]
+    [SerializeField, Tooltip("The color of the highlight")]
+    private Color _color = Color.white;
+    [SerializeField, Tooltip("The width of the highlight contour"), Range(0.01f, 2f)]
+    private float _lineWidth = .2f;
+    [SerializeField, Tooltip("The meshfilter of the object containing the mesh (if found elsewhere)")]
+    private MeshFilter _mesh;
+
+    private GameObject _highlight;
+    public void GazeFocusChanged(bool hasFocus)
+    {
+        _highlight.SetActive(hasFocus);
+    }
+
+    private void Start()
+    {
+        InitHighlight();
+    }
+
+    private void InitHighlight()
+    {
+        //Instantiation the gameobject
+        _highlight = new GameObject("Highlight");
+        //Adding the meshfilter and copying the mesh from the original object
+        _highlight.AddComponent<MeshFilter>().mesh = 
+            (_mesh == null) ? GetComponent<MeshFilter>().mesh : _mesh.mesh;
+        //Setting up the highlight material
+        Material mat = Gaze_Settings.HighlightMaterial;
+        mat.SetColor("g_vOutlineColor", _color);
+        mat.SetFloat("g_flOutlineWidth", _lineWidth / 100f);
+        _highlight.AddComponent<MeshRenderer>().material = mat;
+        //Setting the transform parent and zeroing it's rot and pos
+        _highlight.transform.SetParent(transform);
+        _highlight.transform.localPosition = Vector3.zero;
+        _highlight.transform.localRotation = Quaternion.identity;
+        //Disable the gameobject
+        _highlight.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Gaze/Gaze_Settings.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze_Settings : MonoBehaviour
+{
+    [Header("Highlight settings")]
+    [SerializeField, Tooltip("The base material used for drawing highlights")]
+    private Material _highlightMaterial;
+    public static Material HighlightMaterial 
+    { 
+        get { return new Material(_instance._highlightMaterial); } 
+    }
+
+    private static Gaze_Settings _instance;
+    public static Gaze_Settings Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
