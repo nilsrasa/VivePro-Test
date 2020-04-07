f6c4eed26ec100861555c3442be26a4204f65911
nilsras <nils.rasamoel@gmail.com>
2020-04-07 13:00:34 +0200

    Added pose and binding
    Added a hand pose for the book_3 object
    Added a custom binding for the hideroom script

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean HideRoom
+        {
+            get
+            {
+                return SteamVR_Actions.default_HideRoom;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
