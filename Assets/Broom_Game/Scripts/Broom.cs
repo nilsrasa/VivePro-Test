ed1aa6ce4f9a7f89c5e582ddcbe1bcdf31823672
nilsras <nils.rasamoel@gmail.com>
2020-04-07 16:08:33 +0200

    Created brooming game

---
+++ Assets/Broom_Game/Scripts/Broom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Broom_Game.Scripts
+{
+    public class Broom : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _broomHead;
+
+        void Awake()
+        {
+            _broomHead.AddComponent<BroomHead>();
+        }
+    }
+
+    public class BroomHead : MonoBehaviour
+    {
+        private void Start()
+        {
+            gameObject.AddComponent<Rigidbody>().isKinematic = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var spot = other.GetComponent<Spot>();
+
+            if (spot != null)
+            {
+                spot.Brush();
+            }
+        }
+    }
+}
---
+++ Assets/Broom_Game/Scripts/Spot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Broom_Game.Scripts
+{
+    public class Spot : MonoBehaviour
+    {
+        private int _health = 100;
+        private bool _isDead;
+        private static int damage = 20;
+
+        public void Brush()
+        {
+            if (_isDead)
+                return;
+
+            _health -= damage;
+
+            var m = GetComponent<Renderer>().material;
+            var c = m.color;
+            m.SetColor("_Color", new Color(c.r, c.g, c.b, _health / 100f));
+
+            GetComponentInChildren<ParticleSystem>().Play();
+
+            if (_health <= 0)
+            {
+                _isDead = true;
+                StartCoroutine(Destroy(GetComponentInChildren<ParticleSystem>()));
+            }
+                
+        }
+
+        private IEnumerator Destroy(ParticleSystem ps)
+        {
+            yield return new WaitWhile(() => ps.isPlaying);
+            Destroy(gameObject);
+        }
+    }
+}
